419defa68e1f343509bbf26b3c27a90bb6a5bab5
Agung <agung.maulana@fore.coffee>
2020-11-16 22:39:58 +0700

    update databasae

--- digital_library.sql
+++ digital_library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 15, 2020 at 05:38 PM
+-- Generation Time: Nov 16, 2020 at 04:38 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.27
 
@@ -39,15 +39,32 @@ CREATE TABLE `buku` (
   `stok` int(255) NOT NULL,
   `deskripsi` text NOT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `id_kategori` int(11) NOT NULL
+  `id_kategori` int(11) NOT NULL,
+  `update_by` int(11) DEFAULT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `buku`
 --
 
-INSERT INTO `buku` (`id_buku`, `judul`, `penulis`, `penerbit`, `isbn`, `eisbn`, `tahun_terbit`, `stok`, `deskripsi`, `image`, `id_kategori`) VALUES
-(1, 'BUKU WEB', 'Mr. Programmer', 'Hikam Pustaka', '000-000-000-000', '000-000-000-000', '2020', 100, 'Sebagai seorang Web Design atau Programmer belajar adalah suatu hal yang \r\ndi haruskan, karena dengan belajar kita akan menjadi seorang yang ahli di bidang \r\nWeb Design atau Programmer. Belajar bisa di lakukan dengan banyak cara, contohnya \r\nseperti searching di internet tentang tutorial, artikel, video yang berhubungan dengan \r\napa yang kita mau pelajari, atau bertanya dengan teman yang sudah ahli di bidang yang \r\ningin kita pelajari, atau mencari buku-buku referensi di toko-toko buku untuk mempelajari \r\napa yang ingin kita pelajari, dan sebagainya.', 'buku1.png', 1);
+INSERT INTO `buku` (`id_buku`, `judul`, `penulis`, `penerbit`, `isbn`, `eisbn`, `tahun_terbit`, `stok`, `deskripsi`, `image`, `id_kategori`, `update_by`, `updated_at`) VALUES
+(1, 'BUKU WEB', 'Mr. Programmer', 'Hikam Pustaka', '000-000-000-001', '000-000-000-000', '2020', 97, 'Sebagai seorang Web Design atau Programmer belajar adalah suatu hal yang di haruskan, karena dengan belajar kita akan menjadi seorang yang ahli di bidang Web Design atau Programmer. Belajar bisa di lakukan dengan banyak cara, contohnya seperti searching di internet tentang tutorial, artikel, video yang berhubungan dengan apa yang kita mau pelajari, atau bertanya dengan teman yang sudah ahli di bidang yang ingin kita pelajari, atau mencari buku-buku referensi di toko-toko buku untuk mempelajari apa yang ingin kita pelajari, dan sebagainya.', '5f8acad6df275.png', 1, 1, '2020-11-15 18:47:40'),
+(2, 'BUku judul baru', 'tes', 'tes', 'tes', 'tes', '2020', 80, 'tes', '5f8acabb89a7d.png', 2, 1, '2020-11-14 21:24:42'),
+(3, 'tes Judul', 'ds', 'tes', 'tes', 'tes', '2019', 13, 'tes', '5f8adb1076751.png', 2, 1, '2020-11-15 18:47:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id_cart` int(11) NOT NULL,
+  `buku` varchar(255) NOT NULL,
+  `member` int(11) DEFAULT NULL,
+  `update_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -57,17 +74,19 @@ INSERT INTO `buku` (`id_buku`, `judul`, `penulis`, `penerbit`, `isbn`, `eisbn`, 
 
 CREATE TABLE `kategori` (
   `id_kategori` int(11) NOT NULL,
-  `kategori` varchar(255) NOT NULL
+  `kategori` varchar(255) NOT NULL,
+  `update_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `kategori`
 --
 
-INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
-(1, 'website'),
-(2, 'mobile'),
-(3, 'acounting');
+INSERT INTO `kategori` (`id_kategori`, `kategori`, `update_by`) VALUES
+(1, 'website', NULL),
+(2, 'mobile', NULL),
+(3, 'acounting', NULL),
+(16, 'tes', NULL);
 
 -- --------------------------------------------------------
 
@@ -78,16 +97,16 @@ INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
 CREATE TABLE `kurir` (
   `id_kurir` int(255) NOT NULL,
   `kurir` varchar(255) NOT NULL,
-  `biaya` int(255) DEFAULT NULL
+  `biaya` int(255) DEFAULT NULL,
+  `update_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `kurir`
 --
 
-INSERT INTO `kurir` (`id_kurir`, `kurir`, `biaya`) VALUES
-(1, 'Kurir Digital Library', 20000),
-(2, 'edit', 90002);
+INSERT INTO `kurir` (`id_kurir`, `kurir`, `biaya`, `update_by`) VALUES
+(1, 'Kurir Digital Library', 20000, 0);
 
 -- --------------------------------------------------------
 
@@ -102,16 +121,19 @@ CREATE TABLE `members` (
   `no_wa` varchar(255) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `alamat` text DEFAULT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `status_pekerjaan` varchar(50) DEFAULT NULL,
+  `jenis_kelamin` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`id_member`, `nama`, `email`, `no_wa`, `image`, `alamat`, `password`) VALUES
-(1, 'Agung', 'agung040100@gmail.com', '088704145010', '-', 'jl jalan', ''),
-(2, 'Hanif', 'hanif@gmail.com', '088704145010', '-', 'jl jalan', '');
+INSERT INTO `members` (`id_member`, `nama`, `email`, `no_wa`, `image`, `alamat`, `password`, `status_pekerjaan`, `jenis_kelamin`) VALUES
+(1, 'Agung', 'agung040100@gmail.com', '+62 895-7009-96565', '-', 'jl kesana kesini', '', 'mahasiswa', 'Laki-Laki'),
+(2, 'Hanif', 'hanif@gmail.com', '088704145010', '-', 'jl jalan-jalan aja', '', 'mahasiswa', 'Laki-Laki'),
+(3, 'Sumi', 'sumi@gmail.com', '088704145111', '-', 'jl jalan terus', '', 'mahasiswa', 'Perempuan');
 
 -- --------------------------------------------------------
 
@@ -123,13 +145,78 @@ CREATE TABLE `peminjaman` (
   `id_peminjaman` int(11) NOT NULL,
   `id_buku` varchar(255) NOT NULL,
   `id_member` int(11) NOT NULL,
-  `id_kurir` int(11) NOT NULL,
+  `id_kurir` int(11) DEFAULT NULL,
+  `kirim` tinyint(1) NOT NULL DEFAULT 0,
+  `total_buku` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `tanggal` datetime NOT NULL DEFAULT current_timestamp(),
+  `gambar_bukti` varchar(255) DEFAULT NULL,
+  `tanggal_pinjam` date NOT NULL DEFAULT current_timestamp(),
+  `tanggal_harus_kembali` date DEFAULT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
   `tanggal_update` datetime DEFAULT NULL ON UPDATE current_timestamp(),
   `update_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_peminjaman`, `id_buku`, `id_member`, `id_kurir`, `kirim`, `total_buku`, `status`, `gambar_bukti`, `tanggal_pinjam`, `tanggal_harus_kembali`, `tanggal_kembali`, `tanggal_update`, `update_by`) VALUES
+(1, '1,2', 1, 1, 1, '1,1', 'SELESAI', '-', '2020-10-24', '2020-10-30', '2020-10-24', '2020-11-07 21:00:31', 0),
+(2, '1,2', 1, 1, 1, '1,1', 'SELESAI', '-', '2020-10-24', '2020-10-30', '2020-10-24', '2020-11-07 21:00:38', 0),
+(3, '3', 3, 1, 1, '1', 'BATAL', '-', '2020-10-24', '2020-10-30', NULL, '2020-11-07 20:59:49', 0),
+(4, '3', 3, 1, 1, '2', 'KONFIRMASI', '-', '2020-10-24', '2020-10-30', NULL, '2020-11-02 22:14:02', 0),
+(5, '3', 3, 1, 1, '2', 'BATAL', '-', '2020-10-24', '2020-10-30', '2020-11-14', '2020-11-14 21:31:24', 0),
+(6, '3', 3, 1, 1, '2', 'DIKIRIM', '-', '2020-10-24', '2020-10-30', NULL, '2020-11-15 18:46:41', 0),
+(7, '3', 3, 1, 1, '2', 'TERKIRIM', '-', '2020-10-24', '2020-10-30', NULL, '2020-11-02 22:14:02', 0),
+(8, '1,2', 1, 1, 1, '1,1', 'SELESAI', '-', '2020-10-24', '2020-10-30', '2020-11-14', '2020-11-14 21:22:48', 0),
+(9, '1', 1, NULL, 0, '1', 'SELESAI', NULL, '2020-11-14', NULL, '2020-11-14', '2020-11-14 21:24:32', 1),
+(10, '2,3,1', 1, NULL, 0, '1,1,1', 'DIPINJAM', NULL, '2020-11-14', NULL, NULL, NULL, 1),
+(11, '2', 2, NULL, 0, '3', 'DIPINJAM', NULL, '2020-11-14', NULL, NULL, NULL, 1),
+(12, '1', 3, NULL, 0, '1', 'DIPINJAM', NULL, '2020-11-14', NULL, NULL, NULL, 1),
+(13, '1', 3, NULL, 0, '1', 'DIPINJAM', NULL, '2020-11-14', NULL, NULL, NULL, 1),
+(14, '3,2,1', 1, NULL, 0, '1,1,1', 'SELESAI', NULL, '2020-11-14', '2020-11-03', '2020-11-14', '2020-11-14 21:00:59', 1),
+(15, '1', 1, NULL, 0, '1', 'DIPINJAM', NULL, '2020-11-14', '2020-11-19', NULL, NULL, 1),
+(16, '2', 1, NULL, 0, '3', 'SELESAI', NULL, '2020-11-14', '2020-11-18', '2020-11-14', '2020-11-14 21:24:42', 1),
+(17, '1', 2, NULL, 0, '1', 'DIPINJAM', NULL, '2020-11-15', '2020-11-19', NULL, NULL, 1),
+(18, '3,1', 3, NULL, 0, '1,1', 'DIPINJAM', NULL, '2020-11-15', '2020-11-20', NULL, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengunjung`
+--
+
+CREATE TABLE `pengunjung` (
+  `id_pengunjung` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `jenis_kelamin` varchar(50) NOT NULL,
+  `keperluan` varchar(255) NOT NULL,
+  `tanggal_kunjungan` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengunjung`
+--
+
+INSERT INTO `pengunjung` (`id_pengunjung`, `nama`, `alamat`, `status`, `jenis_kelamin`, `keperluan`, `tanggal_kunjungan`) VALUES
+(1, 'Agung', 'ciluar', 'Mahasiswa', 'Laki-Laki', 'Belajar', '2020-11-15 00:00:00'),
+(2, 'tes', 'tes', 'tes', 'Laki-Laki', 'tes', '2020-11-15 00:00:00'),
+(3, 'Agung', 'ciluar1', 'Mahasiswa', 'Perempuan', 'Belajar', '2020-11-15 00:00:00'),
+(4, 'tes', 'ciluar1', 'Mahasiswa', 'Perempuan', 'ds', '2020-11-15 00:00:00'),
+(6, 'Hanif', 'jl jalan-jalan aja', 'mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 00:00:00'),
+(7, 'Sumi', 'jl jalan terus', 'mahasiswa', 'Perempuan', 'tes', '2020-11-15 00:00:00'),
+(8, 'Agung', 'jl kesana kesini', 'mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 00:00:00'),
+(9, 'Ridho', 'Cirimekar', 'Mahasiswa', 'Laki-Laki', 'Belajar', '2020-11-15 00:00:00'),
+(10, 'Agung', 'jl kesana kesini', 'mahasiswa', 'Laki-Laki', 'Belajar', '2020-11-15 00:00:00'),
+(11, 'tesdaftar', 'test', 'Mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 00:00:00'),
+(12, 'Agung Maulana', 'ciluar', 'Mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 22:45:45'),
+(13, 'tes', 'ciluar1', 'Mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 22:58:52'),
+(14, 'Agung', 'ciluar1', 'Mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 22:59:12'),
+(15, 'Agung', 'ciluar1', 'Mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 22:59:48'),
+(16, 'Agung', 'ciluar1', 'Mahasiswa', 'Laki-Laki', 'tes', '2020-11-15 22:59:54');
 
 -- --------------------------------------------------------
 
@@ -166,15 +253,18 @@ CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `jumlah_login` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `nama`, `username`, `password`) VALUES
-(1, 'Admin', 'admin', '123456');
+INSERT INTO `user` (`id_user`, `nama`, `username`, `password`, `jumlah_login`) VALUES
+(1, 'Admin', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 13),
+(2, 'Admin2', 'admin2', 'e10adc3949ba59abbe56e057f20f883e', NULL),
+(3, 'Agung M', 'm4gung', 'e10adc3949ba59abbe56e057f20f883e', 1);
 
 --
 -- Indexes for dumped tables
@@ -185,6 +275,12 @@ INSERT INTO `user` (`id_user`, `nama`, `username`, `password`) VALUES
 --
 ALTER TABLE `buku`
   ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id_cart`);
 
 --
 -- Indexes for table `kategori`
@@ -211,6 +307,12 @@ ALTER TABLE `peminjaman`
   ADD PRIMARY KEY (`id_peminjaman`);
 
 --
+-- Indexes for table `pengunjung`
+--
+ALTER TABLE `pengunjung`
+  ADD PRIMARY KEY (`id_pengunjung`);
+
+--
 -- Indexes for table `tb_anggota`
 --
 ALTER TABLE `tb_anggota`
@@ -220,7 +322,8 @@ ALTER TABLE `tb_anggota`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -230,13 +333,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `kurir`
@@ -248,13 +357,19 @@ ALTER TABLE `kurir`
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `peminjaman`
 --
 ALTER TABLE `peminjaman`
-  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `pengunjung`
+--
+ALTER TABLE `pengunjung`
+  MODIFY `id_pengunjung` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tb_anggota`
@@ -266,7 +381,7 @@ ALTER TABLE `tb_anggota`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
